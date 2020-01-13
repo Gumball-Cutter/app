1d2d5669a1555ccf18518239711b10cc40f25288
Kagers Spears <kaegunj@gmail.com>
2020-01-13 12:33:56 -0600

    Added notes

---
+++ app/src/main/java/com/example/newapp/MainActivity.kt
@@ -0,0 +1,37 @@
+fun main() {
+
+}
+
+/*
+Single Character
+    val characterA: Char = 'a'
+
+string
+    cal stringWord: String = "Word"
+
+Type Inference
+    val characterA = 'a'
+    val stringWord = "Word"
+
+concatenation
+    var message = "Hello" + " My name is "
+    val name + "Name Josiah"
+    message += name
+        "Hello my name is Name Josiah"
+
+
+    val exclamationMark: Char = '!'
+    message += exclamationMark
+        "Hello my name is Name Josiah!"
+
+String Templates
+    message = "Hello my name is $name"
+
+     println("Hello WW3")
+
+     Git Repository (This is fo the local one)
+        Select VCS -> Import into Version Control -> Create Git repository
+        Open
+        Click Version Control at bottom of Window
+        View Log
+ */
